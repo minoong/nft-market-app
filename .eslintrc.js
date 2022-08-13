a601dcc48bf56b66c2a5498eb321ef594c3d75e4
이민우 <59505318+minoong@users.noreply.github.com>
2022-08-13 14:28:45 +0900

    Revert "feat(#16) vite version 3 with storybook 적용"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,73 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
+ env: {
+  browser: true,
+  es2021: true,
+  jest: true,
+ },
+ extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime', 'plugin:prettier/recommended', 'plugin:cypress/recommended'],
+ parser: '@typescript-eslint/parser',
+ parserOptions: {
+  ecmaFeatures: {
+   jsx: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime', 'plugin:prettier/recommended', 'plugin:cypress/recommended', 'plugin:storybook/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+ },
+ plugins: ['react', 'react-hooks', '@typescript-eslint', 'simple-import-sort'],
+ rules: {
+  '@typescript-eslint/no-unused-vars': ['error'],
+  'import/no-extraneous-dependencies': 'off',
+  'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  'import/extensions': [
+   'error',
+   'ignorePackages',
+   {
+    js: 'never',
+    jsx: 'never',
+    ts: 'never',
+    tsx: 'never',
+    json: 'never',
+    'd.ts': 'never',
+   },
+  ],
+  'no-shadow': 'off',
+  'simple-import-sort/imports': [
+   'error',
+   {
+    groups: [
+     // Packages `react` related packages come first.
+     ['^react', '^@?\\w'],
+     // Internal packages.
+     //  ['^(@|components)(/.*|$)'],
+     // Side effect imports.
+     ['^\\u0000'],
+     // Parent imports. Put `..` last.
+     ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+     // Other relative imports. Put same-folder imports and `.` last.
+     ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+     // Style imports.
+     ['^.+\\.?(css)$'],
+    ],
+   },
+  ],
+  'simple-import-sort/exports': 'error',
+  'react-hooks/rules-of-hooks': 'error',
+  'react-hooks/exhaustive-deps': 'warn',
+  'import/prefer-default-export': 'off',
+  'class-methods-use-this': 'off',
+ },
+ settings: {
+  react: {
+   version: 'detect',
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'simple-import-sort'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error'],
-    'import/no-extraneous-dependencies': 'off',
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }],
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-      json: 'never',
-      'd.ts': 'never'
-    }],
-    'no-shadow': 'off',
-    'simple-import-sort/imports': ['error', {
-      groups: [// Packages `react` related packages come first.
-      ['^react', '^@?\\w'], // Internal packages.
-      //  ['^(@|components)(/.*|$)'],
-      // Side effect imports.
-      ['^\\u0000'], // Parent imports. Put `..` last.
-      ['^\\.\\.(?!/?$)', '^\\.\\./?$'], // Other relative imports. Put same-folder imports and `.` last.
-      ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'], // Style imports.
-      ['^.+\\.?(css)$']]
-    }],
-    'simple-import-sort/exports': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'import/prefer-default-export': 'off',
-    'class-methods-use-this': 'off'
+  'import/parsers': {
+   '@typescript-eslint/parser': ['.ts', '.tsx'],
   },
-  settings: {
-    react: {
-      version: 'detect'
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', 'd.ts']
-      },
-      typescript: './tsconfig.json'
-    }
-  }
-};
+  'import/resolver': {
+   node: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', 'd.ts'],
+   },
+   typescript: './tsconfig.json',
+  },
+ },
+}
